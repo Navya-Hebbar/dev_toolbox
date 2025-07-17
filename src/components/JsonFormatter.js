feb0d7ca7833488d15d3c3b1793994a6c3ea12d0
Navya-Hebbar <navyahebbar032005@gmail.com>
2025-07-17 17:56:14 +0530

    final touches - project deployed

--- src/components/JsonFormatter.js
+++ src/components/JsonFormatter.js
@@ -16,7 +16,8 @@ function JsonFormatter() {
     setShowSuccess(false);
     setOutput("");
     try {
-      const response = await fetch("http://localhost:5000/format-json", {
+      const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000";
+      const response = await fetch(`${apiUrl}/format-json`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ json: input }),
